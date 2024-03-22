5798f2886f8232b67cc8f80f12d370f26108c3f6
Augustine Godwin <129863865+chuks1234567890@users.noreply.github.com>
2024-03-22 18:59:44 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './App.css';
 import App from './App';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { DataProvider } from './context/DataContext';
 const root = ReactDOM.createRoot(document.querySelector('.container '));
 root.render(
   <React.StrictMode>
