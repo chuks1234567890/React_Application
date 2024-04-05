48794e349f5990b3f3abec4f4293c663e293a69d
Augustine Godwin <129863865+chuks1234567890@users.noreply.github.com>
2024-04-05 10:39:15 +0100

    Update nav.js

--- src/nav.js
+++ src/nav.js
@@ -4,9 +4,9 @@ const Nav = () => {
   return (
     <nav>
       <ul>
-        <li title="home"><Link to={"/React_Application"}><i className="fa-solid fa-home"></i></Link></li>
-        <li title="Add blogs"><Link to={"/React_Application/post"}><i className="fa-solid fa-circle-plus"></i></Link></li>
-        <li title="about us"><Link to={"/React_Application/about"}><i className="fa-solid fa-circle-info"></i></Link></li>
+        <li title="React home"><Link to={"/React_Application"}><i className="fa-solid fa-home"></i></Link></li>
+        <li title="React Add blogs"><Link to={"/React_Application/post"}><i className="fa-solid fa-circle-plus"></i></Link></li>
+        <li title="React about us"><Link to={"/React_Application/about"}><i className="fa-solid fa-circle-info"></i></Link></li>
       </ul>
     </nav>
   )
